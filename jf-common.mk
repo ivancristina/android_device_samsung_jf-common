6beae674f2bc35a44a59b9b78c393f3959c3058a
ItsOnlyBinary <github@itsonlybinary.com>
2017-09-11 14:12:45 +0100

    Disable Treble OMX by default.

--- jf-common.mk
+++ jf-common.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
     libstagefrighthw
+    
+# Default OMX service to non-Treble
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
 
 # Power
 PRODUCT_PACKAGES += \
