126ba09cbb918c493ca6285a2f7d051d4ca9b148
tsvietOK <tsvetkov_vlad@ukr.net>
2017-09-15 18:44:53 +0300

    Add Audio HIDL

--- jf-common.mk
+++ jf-common.mk
@@ -82,6 +82,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+	
+# Audio HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
